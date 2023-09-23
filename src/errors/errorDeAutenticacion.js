981cdb9f90064eed227be6148e56e7eaee29f072
aranza318 <akadikinesiologia@hotmail.com>
2023-09-23 18:11:59 -0300

    okeyy

--- src/errors/errorDeAutenticacion.js
+++
@@ -1,6 +0,0 @@
-export class ErrorDeAutenticacion extends Error {
-    constructor(mensaje = 'error de autenticacion') {
-        super(mensaje);
-        this.tipo = 'ERROR_DE_AUTENTICACION';
-    }
-}
--- src/errors/errorDePermisos.js
+++
@@ -1,6 +0,0 @@
-export class ErrorDePermisos extends Error {
-    constructor(mensaje = 'error de permisos') {
-        super(mensaje);
-        this.tipo = 'ERROR_DE_PERMISOS';
-    }
-}
